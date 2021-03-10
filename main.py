d84e8659e2e8b1e85dcbf619f5c915e29f2ded49
Shubhasai <mshubhasai2002@gmail.com>
2021-03-10 15:28:18 +0530

    Version-1.2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 import speech_recognition as sr
 import pyttsx3
 import pywhatkit
@@ -6,22 +7,34 @@ import wikipedia
 import pyjokes
 import requests 
 import bs4
+import threading
+import os
+#threading
+def threading1(): 
+    t1= threading.Thread (target = run_tom) 
+    t1.start()
 listener = sr.Recognizer()
 engine = pyttsx3.init()
+#exit function
+def stop():
+    os._exit(0)
 def talk(text):
     engine.say(text)
     engine.runAndWait()
-print('welcome')
-engine.say('Hey I am tom')
-engine.say('What i can do for you')
-engine.runAndWait()
 def take_command():
+    global dis
+    global label
+    print('welcome')
+    engine.say('Hey I am tom, You can ask me anything. But if you want to quit say  STOP  ')
+    engine.say('What i can do for you')
+    engine.runAndWait()
     try:
         with sr.Microphone(device_index=0) as source:
-            print('Listening...')
+            label = Label(dis, text = " Listening..........").place(x = 40, y = 60)
             voice = listener.listen(source,timeout=1, phrase_time_limit=5)
             command = listener.recognize_google(voice)
             command=command.lower()
+            label = Label(dis, text = "                            ").place(x = 40, y = 60)
     except:
         print("Say that again please...")  
         return "None"
@@ -40,7 +53,7 @@ def run_tom():
         print(time)
     elif 'who is' in command:
         preson=command.replace('who is','')
-        info = wikipedia.summary(preson,3)
+        info = wikipedia.summary(preson,2)
         print(info)
         talk(info)
         pywhatkit.search(preson)
@@ -48,15 +61,13 @@ def run_tom():
         text= preson
         url = 'https://google.com/search?q=' + text
         request_result=requests.get( url )
-        soup = bs4.BeautifulSoup(request_result.text,"html.parser")
-        heading_object=soup.find_all('h3')
-        for info in heading_object: 
-            talk(info.getText())
-            print("------")
-            break
+        request_result=requests.get( url )
+        soup = bs4.BeautifulSoup(request_result.text, 'lxml') 
+        des = soup.get_text()
+        talk(des[230:700])
     elif 'what is' in command:
         product=command.replace('what is','')
-        info = wikipedia.summary(product,5)
+        info = wikipedia.summary(product,2)
         print(info)
         talk(info)
         pywhatkit.search(product)
@@ -64,12 +75,10 @@ def run_tom():
         text= product
         url = 'https://google.com/search?q=' + text
         request_result=requests.get( url )
-        soup = bs4.BeautifulSoup(request_result.text,"html.parser")
-        heading_object=soup.find_all('h3' )
-        for info in heading_object: 
-            talk(info.getText())
-            print("------")
-            break
+        request_result=requests.get( url )
+        soup = bs4.BeautifulSoup(request_result.text, 'lxml') 
+        des = soup.get_text()
+        talk(des[230:700])
     elif 'what are' in command:
         product=command.replace('what are','')
         info = wikipedia.summary(product,2)
@@ -80,24 +89,11 @@ def run_tom():
         text= product
         url = 'https://google.com/search?q=' + text
         request_result=requests.get( url )
-        soup = bs4.BeautifulSoup(request_result.text,"html.parser")
-        heading_object=soup.find_all('h3')
-        for info in heading_object: 
-            talk(info.getText())
-            print("------")
-            break
-    elif 'find' in command:
-        text=command.replace('','')
-        url = 'https://google.com/search?q=' + text
-        request_result=requests.get( url )
-        soup = bs4.BeautifulSoup(request_result.text,"html.parser")
-        heading_object=soup.find_all('h3')
-        for info in heading_object: 
-            talk(info.getText())
-            print("------")
-            break
+        soup = bs4.BeautifulSoup(request_result.text, 'lxml') 
+        des = soup.get_text()
+        talk(des[230:500])
     elif 'who are you' in command:
-        talk('I am made by S.M with python')
+        talk('I am TOM version 1.2    made by suibhsai with python')
         talk('I am sad that you dont know any thing about me')
         talk('but i know who are you.')
     elif 'who i am' in command:
@@ -106,9 +102,31 @@ def run_tom():
         talk('haha,    Homo sapiens LOL')
     elif 'joke' in command:
         talk(pyjokes.get_joke())
-        print(pyjokes.get_joke())
+        print('ha ha')
+    elif 'stop' in command:
+        exit()
     else:
+        user_name = Label(dis, text = "Please try again").place(x = 40, y = 60)
         talk('sorry, i could not understand')
         talk('please try again')
-while True:
-    run_tom()
+        label = Label(dis, text = "                                      ").place(x = 40, y = 60)
+dis = Tk()
+dis.title("TOM_Version_1.1")
+label = Label(dis, text ="Hey I am Your Assistance!" , font = ('Helvetica',20)).pack()
+dis.geometry("550x600")
+dis.maxsize(550,600)
+bg = PhotoImage(file = "tom.png")
+log = PhotoImage(file = "logo.png")
+con = PhotoImage(file = "con2.png")
+cont = PhotoImage(file = "con3.png")
+photoimage = con.subsample(10, 10)
+photoimage2 = cont.subsample(10, 10)
+can1 = Canvas( dis, width = 550, height= 600)
+can1.pack(fill = "both", expand = True)
+can1.create_image( 0, 0, image = bg,anchor = "nw")
+can1.create_image( 180, 10, image = log,anchor = "nw")
+button1 = Button( dis, text = "ASK ME ", font = ('Verdana', 15),image = photoimage, bg = 'grey' ,compound = LEFT, command = threading1)
+button1_canvas = can1.create_window( 230, 400,  anchor = "nw", window = button1)
+button2 = Button( dis, text = "Quit!", font = ('Verdana', 15),image = photoimage2, bg = 'grey' ,compound = LEFT, command = stop)
+button2_canvas = can1.create_window( 247, 470,  anchor = "nw", window = button2)
+dis.mainloop()#make gui interactive 
